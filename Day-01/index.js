7a6c0dbee47897068baf1bf98e6db725785d7c43
Ahsan Burki <ahsanburki2126@gmail.com>
2025-06-12 12:49:22 +0500

    day-01 completed of backend

---
+++ Day-01/index.js
@@ -0,0 +1,43 @@
+//For Each
+// let arr1 = [1, 2, , 3, 5, 7];
+// let ans = arr.forEach((value) => {
+//   console.log(value * 4); //[4,8,12,20,28]
+// });
+
+//<======================================================================================================================>
+
+// Map=>creates a new array of the same length as orignal,perform calculations and add the calculated value to the new array each index.
+// let arr2 = [23, 88, 3, 7, 88, 98, 332];
+// let ans = arr.map((value) => {
+//   return value * 4;
+// });
+
+// console.log(ans);
+
+//<=======================================================================================================================>
+
+//Filter ==> filter out a specific element that satisfies any condition and return true and false accordingly if the condition is satisfied it returns true to the interpreter and all the values that are satisfying the conditions are returned to in the new array.
+
+// let arr3 = [22, 43, 12, 78, 33, 21, 76, 47];
+
+// let ans3 = arr3.filter((val) => {
+//   if (val >= 20) {
+//     return true;
+//   }
+
+//   //No need to write else part, javascript do this part by default
+// });
+
+// console.log(ans3);
+
+//<==========================================================================================================================>
+
+//Async Javascript
+
+// async function abcd() {
+//   let blob = await fetch("https://randomuser.me/api/");
+//   let res = await blob.json();
+//   console.log(res);
+// }
+
+// abcd();
