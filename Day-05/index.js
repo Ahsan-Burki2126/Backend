cbbe54243f33a1f575b2cecb70b82737d6f7b4fa
Ahsan Burki <ahsanburki2126@gmail.com>
2025-06-17 19:07:30 +0500

    part 2 of the notes keeping app

--- Day-05/index.js
+++ Day-05/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const { log } = require("console");
+// const { log } = require("console");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,6 +12,14 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   fs.readdir("./files", (err, files) => {
     res.render("index", { files: files });
+  });
+});
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", {
+      filename: req.params.filename,
+      filedata: filedata,
+    });
   });
 });
 app.post("/create", (req, res) => {
@@ -24,4 +32,5 @@ app.post("/create", (req, res) => {
   );
   res.redirect("/");
 });
+
 app.listen(3000);
