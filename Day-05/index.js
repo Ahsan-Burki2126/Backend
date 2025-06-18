76968db96e1cc70a8fcc5a445266d20ce621b8e7
Ahsan Burki <ahsanburki2126@gmail.com>
2025-06-18 20:35:05 +0500

    its actually day 07 and i added the edit feature in the app

--- Day-05/index.js
+++ Day-05/index.js
@@ -1,12 +1,12 @@
+// const { log } = require("console");
 const express = require("express");
 const app = express();
-const path = require("path");
 const fs = require("fs");
-// const { log } = require("console");
+const path = require("path");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -14,14 +14,7 @@ app.get("/", (req, res) => {
     res.render("index", { files: files });
   });
 });
-app.get("/file/:filename", (req, res) => {
-  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
-    res.render("show", {
-      filename: req.params.filename,
-      filedata: filedata,
-    });
-  });
-});
+
 app.post("/create", (req, res) => {
   fs.writeFile(
     `./files/${req.body.title.split(" ").join("")}.txt`,
@@ -31,6 +24,28 @@ app.post("/create", (req, res) => {
     }
   );
   res.redirect("/");
+});
+
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", { filename: req.params.filename, filedata: filedata });
+    console.log(filedata);
+  });
+});
+
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { file: req.params.filename });
+});
+
+app.post("/edit", (req, res) => {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    (err) => {
+      res.redirect("/");
+    }
+  );
+  // console.log(req.body);
 });
 
 app.listen(3000);
