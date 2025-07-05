ef83154c4517ade67c97bd2644191fce31bc4921
Ahsan Burki <ahsanburki2126@gmail.com>
2025-07-05 14:32:10 +0500

    the updated functionality has been added to the app

--- Day-10/index.js
+++ Day-10/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const userModel = require("./models/user");
 const path = require("path");
+const user = require("./models/user");
+const { log } = require("console");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,6 +25,20 @@ app.post("/create", async (req, res) => {
     email,
     image,
   });
+  res.redirect("/read");
+});
+
+app.get("/edit/:userid", async (req, res) => {
+  let user = await userModel.findOne({ _id: req.params.userid });
+  res.render("edit", { user });
+});
+app.post("/update/:userid", async (req, res) => {
+  let { name, email, image } = req.body;
+  let user = await userModel.findOneAndUpdate(
+    { _id: req.params.userid },
+    { name, email, image },
+    { new: true }
+  );
   res.redirect("/read");
 });
 
